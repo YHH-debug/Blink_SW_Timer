ab9a4589fff2d3c7fcfdf2cb3b3a4e77cb6a2df1
Prabhat Bhatt <36540010+iprabhat29@users.noreply.github.com>
2018-12-19 20:26:28 -0500

    Update main.c

--- Blink_Switch_interrupt/main.c
+++ Blink_Switch_interrupt/main.c
@@ -36,23 +36,34 @@ void SW2_ISR(void)
 
 int main(void)
 {
+    // 1. PLL|16MHz|OSC_MAIN
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+    // 2. Peripheral GPIOF Enable
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    // 3. GPIOF PF1|PF2|PF3 Output
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    // 4. GPIOF PF1|PF2|PF3 Write 0
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2| GPIO_PIN_3,0);
+    // 5. Unlock GPIOF PF0
     HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
     HWREG(GPIO_PORTF_BASE + GPIO_O_CR) = 0x1;
+    // 6. GPIOF PF0|PF4 Input
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4);
+    // 7. GPIOF Pad Configuration PF0|PF1 GPIO Strength 8MA STD Weak Pull Up
     GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
+    // 8. Interrupt GPIOF Set PF0  PF4 Falling Edge
     GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4, GPIO_FALLING_EDGE);
 
     //GPIOIntRegister(GPIO_PORTF_BASE,&SW2_ISR);//Register for interrupt on PF0
+    // 9. Interrupt GPIOF Enable
     GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_0);
-
+    // 10. Register Interrupt Function
     GPIOIntRegister(GPIO_PORTF_BASE,&SW1_ISR);
+    // 11. Start GPIOF Interrupt
     GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_4);//Register for interrupt for PF4
-
+    // 12. Enable Interrupt
     IntEnable(INT_GPIOF);
+    // 13. Enable Master Interrupt
     IntMasterEnable();
 while(1)
     {
